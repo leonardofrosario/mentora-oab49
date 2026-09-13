44e495655b60091027957faa00f4ff2b4fb26838
Leonardo Freitas <leonardofrosario@hotmail.com>
2026-09-13 16:31:46 -0300

    Configura conexão pública com Supabase

---
+++ cloud-config.js
@@ -0,0 +1,5 @@
+window.MENTORA_CLOUD = {
+  supabaseUrl: 'https://mxbdzpvtcmhudzpwqhno.supabase.co',
+  supabaseKey: 'sb_publishable_KUEpiwUBKdocnuWSHCdkeA_erAVpqv_',
+  appUrl: 'https://leonardofrosario.github.io/mentora-oab49/'
+};
